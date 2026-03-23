d34a098a26565a8327a5361f96c950cc51c31744
Gosha924 <151758180+Gosha924@users.noreply.github.com>
2026-03-23 20:50:39 +0300

    add typing

--- src/kernels.py
+++ src/kernels.py
@@ -1,10 +1,10 @@
-def blur_kernel():
+def blur_kernel() -> list[list[float]]:
     return [[1/9, 1/9, 1/9], [1/9, 1/9, 1/9], [1/9, 1/9, 1/9]]
 
 def sharpness_kernel():
     return [[0, -1, 0], [-1, 5, -1], [0, -1, 0]]
 
-def emboss_kernel():
+def emboss_kernel() -> list[list[int]]:
     """
     нужно прибавлять к результату 128 чтобы было видно на сером фоне
     """
