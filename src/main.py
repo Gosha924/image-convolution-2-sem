5160bc304ba8024f6398f01851b96c9122a22485
Gosha924 <151758180+Gosha924@users.noreply.github.com>
2026-03-23 20:30:38 +0300

    исправил чтобы black не ругался

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageFilter
 from pathlib import Path
 
+
 def get_image_path(image_name) -> str:
     """
     Возвращает полный путь к изображению в папке images
@@ -10,14 +11,12 @@ def get_image_path(image_name) -> str:
     image_path = project_root / "images" / image_name
     return image_path
 
+
 image = Image.open(get_image_path("image1.jpg"))
-kernel = [0, -1, 0,
-          -1, 5, -1,
-          0, -1 , 0]
+kernel = [0, -1, 0, -1, 5, -1, 0, -1, 0]
 native_filter = image.filter(ImageFilter.CONTOUR)
 my_first_filter = ImageFilter.Kernel((3, 3), kernel)
 result = image.filter(my_first_filter)
 
 native_filter.save("native_fiter.jpeg")
 result.save("my_filter2.jpeg")
-
